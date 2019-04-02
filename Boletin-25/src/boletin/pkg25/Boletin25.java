8e483d90204d27a27cc3760137d7321162bc7ecc
ifernandezblanco <ifernandezblanco@nautilus29.danielcastelao.org>
2019-04-02 12:14:35 +0200

    Pro-Boletin-25

---
+++ Boletin-25/src/boletin/pkg25/Boletin25.java
@@ -0,0 +1,28 @@
+
+package boletin.pkg25;
+
+/**
+ *
+ * @author ifernandezblanco
+ */
+public class Boletin25 {
+  
+        public static Ventana2 obxven2 = new Ventana2();
+        
+   
+   
+        public static Ventana1 obxven = new Ventana1();
+        
+    
+    public static void main(String[] args) {
+
+obxven2.setVisible(true);
+        obxven.setVisible(true);
+        
+       
+        
+      
+    
+    }
+    
+}
